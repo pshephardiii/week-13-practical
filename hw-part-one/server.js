aba1e32b764d061682f8a20cfeb635e878d1fd61
Paul Shephard <paulshephard598@gmail.com>
2023-12-19 16:42:59 -0500

    Tip Calculator express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Greetings App
+
 app.get('/greeting', (req, res) => {
     res.send('<h1>Hello Stranger</h1>')
 })
 
 app.get('/greeting/:name', (req, res) => {
     res.send(`<h1>Hello ${req.params.name}</h1>`)
+})
+
+// Tip Calculator App
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`<h1>${req.params.total * (req.params.tipPercentage/100)}</h1>`)
 })
 
 
