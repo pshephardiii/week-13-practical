582bb68440d05d0842d48075bf0af24061bd613f
Paul Shephard <paulshephard598@gmail.com>
2023-12-19 16:58:48 -0500

    Magic 8 Ball express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Array for 8 Ball App
+const magicArr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
 // Greetings App
 
 app.get('/greeting', (req, res) => {
@@ -16,6 +19,12 @@ app.get('/greeting/:name', (req, res) => {
 
 app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`<h1>${req.params.total * (req.params.tipPercentage/100)}</h1>`)
+})
+
+// Magic 8 Ball App
+
+app.get('/magic/:question', (req, res) => {
+    res.send(`<h2>${req.params.question}?</h2><h1>${magicArr[Math.floor(Math.random() * 20)]}</h1>`)
 })
 
 
